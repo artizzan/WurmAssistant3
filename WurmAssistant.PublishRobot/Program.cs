2654185335622a3e0fe06d1d041715090af25be5
artizzan <artizzan7@gmail.com>
2015-08-20 14:08:12 +0200

    added PublishRobot project

---
+++ WurmAssistant.PublishRobot/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace AldursLab.WurmAssistant.PublishRobot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Console write out");
+            Trace.WriteLine("Console write out");
+        }
+    }
+}
